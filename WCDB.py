31c95d4e1027e8906e42da16406d85d7de52debf
Robert Salinas <robert.salinas.91@gmail.com>
2014-03-30 17:39:05 -0500

    Updated all table queries

--- WCDB.py
+++ WCDB.py
@@ -73,7 +73,7 @@ def createDB(login):
         t = wcdb_query(login, "drop table if exists Student;")
         assert(t is None)
         
-        wcdb_query(login, "drop table if exists crises;")
+    wcdb_query(login, "drop table if exists crises;")
 	wcdb_query(login, "drop table if exists orgs;")
 	wcdb_query(login, "drop table if exists people")
 	wcdb_query(login, "drop table if exists resources;")
@@ -130,18 +130,216 @@ def createDB(login):
                 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
                 """)
         # he defines crisisID in schema, now it becomes a bigint, I got lost, anyway, just following at this time being
+	
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE Orgs (
+                orgsId bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                name text COLLATE utf8_unicode_ci NOT NULL,
+                kind enum('Corporation','Government Agency','Military Force','Intergovernmental Agency','Intergovernmental Public Health Agency', 'Nonprofit / Humanitarian Organization') COLLATE utf8_unicode_ci NOT NULL,
+                streetAddress text COLLATE utf8_unicode_ci DEFAULT NULL,
+                city text COLLATE utf8_unicode_ci DEFAULT NULL,
+                stateOrProvince text COLLATE utf8_unicode_ci DEFAULT NULL,
+                postalCode text COLLATE utf8_unicode_ci DEFAULT NULL,
+                country text COLLATE utf8_unicode_ci DEFAULT NULL,
+                foundingMission text COLLATE utf8_unicode_ci DEFAULT NULL,
+                datefounded datetime NOT NULL,
+                majorEvents text COLLATE utf8_unicode_ci DEFAULT NULL,
+                PRIMARY KEY (orgsId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
+                
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE People (
+                peopleId bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                name text COLLATE utf8_unicode_ci NOT NULL,
+                kind enum('President','Celebrity','Actor/Actress','Musician','Politician', 'CEO','Humanitarian','Perpetrator','Regular Worker') COLLATE utf8_unicode_ci NOT NULL,
+                streetAddress text COLLATE utf8_unicode_ci DEFAULT NULL,
+                city text COLLATE utf8_unicode_ci DEFAULT NULL,
+                stateOrProvince text COLLATE utf8_unicode_ci DEFAULT NULL,
+                postalCode text COLLATE utf8_unicode_ci DEFAULT NULL,
+                country text COLLATE utf8_unicode_ci DEFAULT NULL,
+                
+
+                PRIMARY KEY (peopleId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
+                
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE Resources (
+                resourceId bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                resource text COLLATE utf8_unicode_ci NOT NULL,
+                PRIMARY KEY (resourceId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
+
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE CrisisResources (
+                crisisId bigint(20) unsigned NOT NULL,
+                resourceId bigint(20) unsigned NOT NULL,
+                PRIMARY KEY (crisisId,resourceID)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
+
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE WaysToHelp (
+                helpId bigint(20) unsigned NOT NULL,
+                wayToHelp text COLLATE utf8_unicode_ci NOT NULL,
+                PRIMARY KEY (helpId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
+
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE CrisisWaysToHelp (
+                crisisId bigint(20) unsigned NOT NULL,
+                helpId bigint(20) unsigned NOT NULL,
+                PRIMARY KEY (crisisId, helpId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE ContactInfos (
+                contactInfoId bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                phoneNumber text COLLATE utf8_unicode_ci DEFAULT NULL,
+                emailAddress text COLLATE utf8_unicode_ci DEFAULT NULL,
+                facebookUrlId bigint(20) unsigned DEFAULT NULL,
+                websiteUrlId text bigint(20) unsigned DEFAULT NULL,
+                PRIMARY KEY (contactInfoId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
+                
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE orgContactInfos (
+                orgId bigint(20) unsigned NOT NULL,
+                contactInfoId bigint(20) unsigned NOT NULL,
+                PRIMARY KEY (orgId, ContactInfoId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
+                
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE Citations (
+                citationId bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                citation text COLLATE utf8_unicode_ci NOT NULL,
+                PRIMARY KEY (citationId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
+                
 	t = wcdb_query(
                 login,
 		"""
                 CREATE TABLE CrisisCitations (
-                crisisId bigint(20) unsigned NOT NULL,
                 citationId bigint(20) unsigned NOT NULL,
-                PRIMARY KEY (crisisId,citationId)
-                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-                """) 
+                crisisId bigint(20) unsigned NOT NULL,
+                PRIMARY KEY (citationId, crisisId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
 
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE OrgCitations (
+            	orgId bigint(20) unsigned NOT NULL,
+                citationId bigint(20) unsigned NOT NULL,
+                PRIMARY KEY (orgId, citationId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
+	
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE PersonCitations (
+                personId bigint(20) unsigned NOT NULL,
+                citationId bigint(20) unsigned NOT NULL,
+                PRIMARY KEY (personId, citationId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
+                
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE Urls (
+                urlId bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                type text COLLATE utf8_unicode_ci NOT NULL,
+                urlAddress text COLLATE utf8_unicode_ci NOT NULL,
+                PRIMARY KEY (urlId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
+                
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE CrisisUrls (
+                crisisId bigint(20) unsigned NOT NULL,
+                urlId bigint(20) unsigned NOT NULL,
+                PRIMARY KEY (crisisId, urlId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
+                
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE OrgUrls (
+                orgId bigint(20) unsigned NOT NULL,
+                urlId bigint(20) unsigned NOT NULL,
+                PRIMARY KEY (orgId, urlId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
+                
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE PersonUrls (
+                personId bigint(20) unsigned NOT NULL,
+                urlId bigint(20) unsigned NOT NULL,
+                PRIMARY KEY (personId, urlId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
+                
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE CrisisOrgs (
+                crisisId bigint(20) unsigned NOT NULL,
+                orgId bigint(20) unsigned NOT NULL,
+                PRIMARY KEY (crisisId, orgId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
 
-
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE CrisisPeople (
+                crisisId bigint(20) unsigned NOT NULL,
+                personId bigint(20) unsigned NOT NULL,
+                PRIMARY KEY (crisisId, personId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
+                
+	t = wcdb_query(
+                login,
+		"""
+                CREATE TABLE OrgPeople (
+                orgId bigint(20) unsigned NOT NULL,
+                personId bigint(20) unsigned NOT NULL,
+                PRIMARY KEY (orgId, personId)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                """)
 def main():
         login_var = wcdb_login(*a) # we just learned this, so....
 
